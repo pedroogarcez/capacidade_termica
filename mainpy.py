8e71b0690ddf486a6931a5024c82cea5a33aedd9
pedroogarcez <gar6pedro@gmail.com>
2022-11-22 12:09:11 -0300

    tkinter window

--- mainpy.py
+++ mainpy.py
@@ -1,6 +1,7 @@
 #Nesse projeto, vamos calcular o calor específico molar de diferentes materiais para o modelo de Debye.
 import scipy.integrate
 import numpy as np
+import tkinter as tk
 
 #Implmentando a função de Debye
 
@@ -15,12 +16,16 @@ def calor_especifico(lista_temperatura):
         valor_x.append(x)
     return
 calor_especifico(lista_temperatura)
-print(valor_x)
+#print(valor_x)
 
 #for i in range(len(valor_x)):
     #integral, erro = scipy.integrate.quad()
 
 d = lambda u: (u**4 * np.exp(u))/(np.exp(u)*1)**2
 integral = scipy.integrate.quad(d,1,5)
-print(integral)
+#print(integral)
 
+janela = tk.Tk()
+janela.title('Capacidade térmica dos materiais')
+janela.geometry('400x150')
+janela.mainloop()
